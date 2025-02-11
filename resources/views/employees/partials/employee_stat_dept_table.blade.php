1b396c01504023bd6d4d5711817f4816b4a9bba7
pandragama <pan2rafa@gmail.com>
2025-02-11 22:43:53 +0700

    refactor: total status ber-icon

--- resources/views/employees/partials/employee_stat_dept_table.blade.php
+++ resources/views/employees/partials/employee_stat_dept_table.blade.php
@@ -6,7 +6,12 @@
                 <th class="py-2 px-2 text-left">Contract</th>
                 <th class="py-2 px-2 text-left">Employee</th>
                 <th class="py-2 px-2 text-left">Not Active</th>
-                <th class="py-2 pr-6 pl-2 text-left">Grand Total</th>
+                <th class="py-2 pr-6 pl-2 text-left flex items-center gap-1">
+                    <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                        <path fill-rule="evenodd" d="M8 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4H6Zm7.25-2.095c.478-.86.75-1.85.75-2.905a5.973 5.973 0 0 0-.75-2.906 4 4 0 1 1 0 5.811ZM15.466 20c.34-.588.535-1.271.535-2v-1a5.978 5.978 0 0 0-1.528-4H18a4 4 0 0 1 4 4v1a2 2 0 0 1-2 2h-4.535Z" clip-rule="evenodd"/>
+                    </svg>                    
+                    Total
+                </th>
             </tr>
         </thead>
         <tbody class="text-gray-600 text-sm font-light dark:text-gray-200">
