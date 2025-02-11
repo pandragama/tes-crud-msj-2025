e9d94f767ac35169aa2fb8dbd3b9036fc676206c
pandragama <pan2rafa@gmail.com>
2025-02-12 00:24:32 +0700

    fix: old address value

--- resources/views/employees/create.blade.php
+++ resources/views/employees/create.blade.php
@@ -73,9 +73,8 @@
                                         dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 
                                         dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                 name="address"
-                                value="{{ old('address') }}"
                                 required autocomplete="current-address"
-                                ></textarea>
+                                >{{ old('address') }}</textarea>
 
 
                             <x-input-error :messages="$errors->get('address')" class="mt-2" />
