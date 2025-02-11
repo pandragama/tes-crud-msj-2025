9d3c0d8117d1e86f8b0fc88b5dc91477b6918894
pandragama <pan2rafa@gmail.com>
2025-02-11 23:44:25 +0700

    kustomisasi doughnut chart

--- resources/views/employees/index.blade.php
+++ resources/views/employees/index.blade.php
@@ -33,8 +33,8 @@
                     </div>
                 </div>
                 <div class="min-w-min lg:flex-none flex-1 flex justify-center items-center px-6 py-4 bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="chart-container relative h-[290px]">
-                        <canvas id="myChart"></canvas>
+                    <div class="chart-container relative">
+                        <canvas id="myChart" width="500" height="250"></canvas>
                     </div>
                 </div>
             </div>
@@ -94,23 +94,45 @@
         }
     </script>
 
+    @php
+    $chartDepartments = array_map(function($item) {
+    return $item['department'];
+    }, $employeeStatusCountsByDepartment->toArray());
+    $chartTotal = array_map(function($item) {
+    return $item['total'];
+    }, $employeeStatusCountsByDepartment->toArray());
+    @endphp
+
     <!--  tell the script to be a module (for chart.js undefined problem) -->
-    <script type="module" > 
+    <script type="module">
+        var labels = @json($chartDepartments);
+        var data = @json($chartTotal);
         var ctx = document.getElementById('myChart').getContext('2d');
         var myChart = new Chart(ctx, {
             type: 'doughnut',
             data: {
-                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                labels: labels,
+                // labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
                 datasets: [{
                     label: '# of Votes',
-                    data: [12, 19, 3, 5, 2, 3],
+                    data: data,
+                    // data: [12, 19, 3, 5, 2, 3],
                     backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)',
+                        'rgba(255, 99, 71, 1)', // Tambahan baru
+                        'rgba(0, 128, 0, 1)', // Tambahan baru
+                        'rgba(0, 191, 255, 1)', // Tambahan baru
+                        'rgba(75, 0, 130, 1)', // Tambahan baru
+                        'rgba(255, 20, 147, 1)', // Tambahan baru
+                        'rgba(255, 165, 0, 1)', // Tambahan baru
+                        'rgba(34, 139, 34, 1)', // Tambahan baru
+                        'rgba(70, 130, 180, 1)', // Tambahan baru
+                        'rgba(105, 105, 105, 1)' // Tambahan baru
                     ],
                     borderColor: [
                         'rgba(255, 99, 132, 1)',
@@ -118,20 +140,62 @@
                         'rgba(255, 206, 86, 1)',
                         'rgba(75, 192, 192, 1)',
                         'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
+                        'rgba(255, 159, 64, 1)',
+                        'rgba(255, 99, 71, 1)', // Tambahan baru
+                        'rgba(0, 128, 0, 1)', // Tambahan baru
+                        'rgba(0, 191, 255, 1)', // Tambahan baru
+                        'rgba(75, 0, 130, 1)', // Tambahan baru
+                        'rgba(255, 20, 147, 1)', // Tambahan baru
+                        'rgba(255, 165, 0, 1)', // Tambahan baru
+                        'rgba(34, 139, 34, 1)', // Tambahan baru
+                        'rgba(70, 130, 180, 1)', // Tambahan baru
+                        'rgba(105, 105, 105, 1)' // Tambahan baru
                     ],
-                    borderWidth: 1
+                    // backgroundColor: [
+                    //     'rgba(255, 99, 132, 0.2)',
+                    //     'rgba(54, 162, 235, 0.2)',
+                    //     'rgba(255, 206, 86, 0.2)',
+                    //     'rgba(75, 192, 192, 0.2)',
+                    //     'rgba(153, 102, 255, 0.2)',
+                    //     'rgba(255, 159, 64, 0.2)'
+                    // ],
+                    // borderColor: [
+                    //     'rgba(255, 99, 132, 1)',
+                    //     'rgba(54, 162, 235, 1)',
+                    //     'rgba(255, 206, 86, 1)',
+                    //     'rgba(75, 192, 192, 1)',
+                    //     'rgba(153, 102, 255, 1)',
+                    //     'rgba(255, 159, 64, 1)'
+                    // ],
+                    // borderWidth: 2
                 }]
             },
             options: {
                 responsive: true,
-                legend: {
-                    position: 'bottom'
+                maintainAspectRatio: false, // Set false untuk mengabaikan rasio aspek
+                // legend: {
+                //     position: 'top'
+                // },
+                plugins: {
+                    legend: {
+                        display: true,
+                        position: 'right', // Ubah posisi legend di sini
+                        labels: {
+                            color: 'rgb(200, 200, 200)',
+                            sort: (a, b, data) => {
+                                // Mendapatkan nilai data dari dataset
+                                const dataset = data.datasets[0];
+                                const valueA = dataset.data[a.index];
+                                const valueB = dataset.data[b.index];
+                                return valueB - valueA; // Mengurutkan secara descending
+                            }
+                        }
+                    }
                 },
-                title: {
-                    display: true,
-                    text: 'Chart.js Doughnut Chart'
-                },
+                // title: {
+                //     display: true,
+                //     text: 'Chart.js Doughnut Chart'
+                // },
                 animation: {
                     animateScale: true,
                     animateRotate: true
